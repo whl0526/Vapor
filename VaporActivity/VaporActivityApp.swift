d90e93593b5920c0330aa13ebe398df371b89dbb
이원형 <67450169+whl0526@users.noreply.github.com>
2022-12-05 21:33:53 +0900

    Initial Commit

---
+++ VaporActivity/VaporActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaporActivityApp.swift
+//  VaporActivity
+//
+//  Created by 이원형 on 2022/12/05.
+//
+
+import SwiftUI
+
+@main
+struct VaporActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
